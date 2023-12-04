efd784f47186eca3cceb5a1b72e356caa79fd0f5
周翔 <keyneko@163.com>
2023-12-05 00:39:49 +0800

    feat: 添加svg图标

--- src/icons/index.js
+++ src/icons/index.js
@@ -6,5 +6,9 @@ import concat from 'lodash'
 Vue.component('SvgIcon', SvgIcon)
 
 const svg = require.context('./svg', false, /\.svg$/)
+const flags = require.context('./flags', false, /\.svg$/)
+const colored = require.context('./colored', false, /\.svg$/)
 const requireAll = requireContext => requireContext.keys().map(requireContext)
 requireAll(svg)
+requireAll(flags)
+requireAll(colored)
